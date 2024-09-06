545fc09f1b727e4078899919ae8fff1d0b817ee7
Chambers37 <103705874+Chambers37@users.noreply.github.com>
2024-09-06 10:52:02 -0400

    removed chat and websockets

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import RegisterLogin from "./components/loginPage.jsx"
 import AboutUsPage from "./components/about-us.jsx"
 import { Routes,Route } from "react-router-dom";
 import GameScene from '../scripts/game.jsx';
-import Chat from "./components/Chat.jsx";
+// import Chat from "./components/Chat.jsx";
 
 const App = () => {
 
@@ -11,11 +11,11 @@ const App = () => {
     <>
       <NavBar/>
           <Routes>
-            <Route exact path="/" element={<RegisterLogin/>}></Route>
+            <Route exact path="/" element={<GameScene />}></Route>
             <Route exact path="/Login" element={<RegisterLogin/>}></Route>
             <Route exact path="/Play" element={<GameScene />}></Route>
             <Route exact path="/About-Us" element={<AboutUsPage/>}></Route>
-            <Route exact path="/Chat" element={<Chat/>}></Route>
+            {/* <Route exact path="/Chat" element={<Chat/>}></Route> */}
           </Routes>
     </>
   )
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,76 +1,76 @@
 
-import { io } from "socket.io-client"
-import { useState, useEffect, useRef } from "react";
-import { useNavigate } from "react-router-dom";
+// import { io } from "socket.io-client"
+// import { useState, useEffect, useRef } from "react";
+// import { useNavigate } from "react-router-dom";
 
-const socket = io.connect("http://localhost:3001");
+// // const socket = io.connect("http://localhost:3001");
 
-const Chat = () => {
-  const [messageReceived,setMessageReceived] = useState([])
-  const [message,setMessage] = useState("")
-  const [username, setUsername] = useState("")
-  const navigate = useNavigate();
-  const  messagesEndRef = useRef(null);
+// const Chat = () => {
+//   const [messageReceived,setMessageReceived] = useState([])
+//   const [message,setMessage] = useState("")
+//   const [username, setUsername] = useState("")
+//   const navigate = useNavigate();
+//   const  messagesEndRef = useRef(null);
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-    const storedUsername = localStorage.getItem('username');
+//   useEffect(() => {
+//     const token = localStorage.getItem('token');
+//     const storedUsername = localStorage.getItem('username');
 
-    if (!token || !storedUsername) {
-      navigate('/Login');
-    } else {
-      setUsername(storedUsername);
-    }
-  }, [navigate]);
-
-
-  const sendMessage = () => {
-    if (message.trim() !== "" && {username, message}) {
-      socket.emit("send_message",{ username, message });
-      setMessageReceived((prevMessage) => [...prevMessage, {username, message}]);
-      setMessage("")
-    }
-  };
+//     if (!token || !storedUsername) {
+//       navigate('/Login');
+//     } else {
+//       setUsername(storedUsername);
+//     }
+//   }, [navigate]);
 
 
-  useEffect(() => {
-    socket.on("receive_message", (data) => {
-      setMessageReceived((prevMessage) => [...prevMessage, {username: data.username, message: data.message}]);
-    });
+//   const sendMessage = () => {
+//     if (message.trim() !== "" && {username, message}) {
+//       socket.emit("send_message",{ username, message });
+//       setMessageReceived((prevMessage) => [...prevMessage, {username, message}]);
+//       setMessage("")
+//     }
+//   };
 
-    return () => {
-      socket.off("receive_message");
-    }
-  }, []);
 
-  useEffect(() => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [messageReceived]);
+//   useEffect(() => {
+//     socket.on("receive_message", (data) => {
+//       setMessageReceived((prevMessage) => [...prevMessage, {username: data.username, message: data.message}]);
+//     });
 
-  return(
-  <section>
-      <div className="msgbox">
-        <h2 id='MessagesTitle'>Messages</h2>
-          <div className="allMessages">
-            <ul className="theList">
-            {messageReceived.map((msg, index) => (
-              <li className='singleMsg' key={index}>{`${msg.username}: ${msg.message}`}</li>
-            ))}
-            </ul>
-          </div>
-      </div>
-      <div className="msgbutton">
-        <input
-          id='msgInput'
-          value={ message }
-          placeholder="Message..."
-          onChange={(event) => {setMessage(event.target.value)}}
-          onKeyDown={(event)=>{if(event.key==="Enter") sendMessage();}}/>
-        <button onClick={sendMessage}>Send Message</button>
-      </div>
-  </section>
-);
+//     return () => {
+//       socket.off("receive_message");
+//     }
+//   }, []);
 
-};
+//   useEffect(() => {
+//     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+//   }, [messageReceived]);
 
-export default Chat
+//   return(
+//   <section>
+//       <div className="msgbox">
+//         <h2 id='MessagesTitle'>Messages</h2>
+//           <div className="allMessages">
+//             <ul className="theList">
+//             {messageReceived.map((msg, index) => (
+//               <li className='singleMsg' key={index}>{`${msg.username}: ${msg.message}`}</li>
+//             ))}
+//             </ul>
+//           </div>
+//       </div>
+//       <div className="msgbutton">
+//         <input
+//           id='msgInput'
+//           value={ message }
+//           placeholder="Message..."
+//           onChange={(event) => {setMessage(event.target.value)}}
+//           onKeyDown={(event)=>{if(event.key==="Enter") sendMessage();}}/>
+//         <button onClick={sendMessage}>Send Message</button>
+//       </div>
+//   </section>
+// );
+
+// };
+
+// export default Chat
